7a84335727b2fdfc7974dfc6c8c8b7463dbc2ba0
Dhaval <dhavallaiya@gmail.com>
2020-03-01 23:52:24 +0530

    Add generating token and middleware

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    }
+  },
   password: {
     type: String,
     required: true,
@@ -11,7 +31,61 @@ const User = mongoose.model('User', {
         throw new Error('Password cannot be \'password\'.');
       }
     }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  if (!email) {
+    throw new Error('Email field is required');
   }
-})
+  if (!password) {
+    throw new Error('Password field is required');
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+}
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisiskey');
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+}
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User;
--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -2,14 +2,48 @@ const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
 
-router.post('/users', (req, res) => {
+router.post('/users', async (req, res) => {
   const user = new User(req.body);
-  user.save().then((response) => {
-    res.status(201).send(response);
-  }).catch((err) => {
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  }
+  catch (err) {
     res.status(400).send(err);
-  })
+  }
 });
+
+router.patch('/user/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowed = ['email', 'password', 'age', 'name'];
+  const isValidOperation = updates.every(update => allowed.includes(update));
+  if (!isValidOperation) {
+    res.status(400).send({ error: 'Invalid updates!' });
+  }
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    updates.forEach(update => user[update] = req.body[update])
+    await user.save();
+    res.send(user);
+  }
+  catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+})
 
 router.get('/users', (req, res) => {
   User.find({}).then((users) => {
